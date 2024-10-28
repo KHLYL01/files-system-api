48354ce2676a09e2b8d554c70672eb4cffa0ad0a
KHLYL01 <kk0026999@gmail.com>
2024-10-28 14:36:22 +0300

    enable cors for whole application

---
+++ src/main/java/com/conquer_team/files_system/config/CorsConfiguration.java
@@ -0,0 +1,20 @@
+package com.conquer_team.files_system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**"); // Enable Cors for whole application
+            }
+        };
+    }
+}
